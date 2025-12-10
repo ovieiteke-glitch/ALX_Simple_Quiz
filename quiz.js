02e42681988f164e8299a6f9b0ce2cf4a9c57d87
Ovie Iteke <ovie.iteke@gmail.com>
2025-12-10 16:48:49 +0100

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,4 +1,25 @@
 //Identify the Correct Answer
 function checkAnswer() {
+    const correctAnswer = "4";
+//Identify the Correct Answer
+function checkAnswer() {
     const correctAnswer = "4"
+    //Retrieve the User’s Answer:
+const userOption = document.querySelector('input[name="quiz"]:checked');    
+const response = document.getElementById("feedback");
+
+if (userOption) {
+    response.textContent;
+    return;
+}
+const userAnswer = userOption.value;
+//Compare the User’s Answer with the Correct Answer:
+if (userAnswer === correctAnswer) {
+    response.textContent = "Correct! Well done";
+} else {
+    response.textContent = "That's incorrect. Try again!";
+}
+}
+//Add an Event Listener to the Submit Button:
+document.getElementById('submit-answer').addEventListener("click", checkAnswer);
 }
